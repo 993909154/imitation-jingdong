fcfa69913762b63b7bd26e36e104f879f8b89673
Along <993909154@qq.com>
2022-02-07 19:16:48 +0800

    feat:bottom docker

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="docker">
+    <span class="docker_item docker_item--active">
+      <div class="iconfont">&#xe692;</div>
+      <div class="docker_title">首页</div>
+    </span>
+    <span class="docker_item">
+      <div class="iconfont">&#xe62e;</div>
+      <div class="docker_title">购物车</div>
+      </span>
+    <span class="docker_item">
+      <div class="iconfont">&#xe713;</div>
+      <div class="docker_title">订单</div>
+    </span>
+    <span class="docker_item">
+      <div class="iconfont">&#xe7ae;</div>
+      <div class="docker_title">我的</div>
+    </span>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  display: flex;
+  position: absolute;
+  box-sizing: border-box;
+  padding: .07rem .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: 1px solid #F1F1F1;
+  box-shadow: 0 -1px 1px 0 #F1F1F1;
+  &_item {
+    flex: 1;
+    text-align: center;
+    color: #333333;
+    .iconfont {
+      margin: 0 0 .02rem 0;
+      font-size: .18rem;
+    }
+    &--active {
+      color: #1FA4FC
     }
   }
+  &_title {
+    font-size: 20px;
+    //浏览器所能显示的最小像素为12px，如果要使用小于12px的像素，请使用transform
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
 }
+
 </style>
