883aa86ef224dff1e4a7f2cbba9ca73d89284ae1
Along <993909154@qq.com>
2022-02-08 00:05:08 +0800

    css:home page layout

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,165 @@
       北京理工大学国防科技园2号楼10层
       <span class="iconfont position_notice">&#xe613;</span>
     </div>
+    <div class="search">
+      <span class="iconfont">&#xe741;</span>
+      <span>山姆会员商店优惠商品</span>
+    </div>
+    <div class="banner">
+      <img class="banner_img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt="">
+    </div>
+    <div class="icons">
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">超市便利</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/菜市场.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">菜市场</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/水果店.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">水果店</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/鲜花.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">鲜花</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/医药健康.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">医药健康</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/家居.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">家居时尚</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/蛋糕.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">烘培蛋糕</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/签到.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">签到</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/大牌免运.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">大牌免运</p>
+      </div>
+      <div class="icons_item">
+        <img src="http://www.dell-lee.com/imgs/vue3/红包.png" alt="" class="icons_item_img">
+        <p class="icons_item_desc">红包套餐</p>
+      </div>
+    </div>
+    <div class="gap"></div>
+    <div class="nearby">
+      <h3 class="nearby_title">附近店铺</h3>
+      <div class="nearby_item">
+        <img class="nearby_item_img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="">
+        <div class="nearby_item_content">
+          <div class="nearby_item_content_title">沃尔玛</div>
+          <div class="nearby_item_content_tags">
+            <span class="nearby_item_content_tag">月售1万+</span>
+            <span class="nearby_item_content_tag">起送¥0</span>
+            <span class="nearby_item_content_tag">基础运费¥5</span>
+          </div>
+          <p class="nearby_item_content_highLight">VIP尊享满89元减4元运费券（每月3张）</p>
+        </div>
+      </div>
+      <div class="nearby_item">
+        <img class="nearby_item_img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="">
+        <div class="nearby_item_content">
+          <div class="nearby_item_content_title">沃尔玛</div>
+          <div class="nearby_item_content_tags">
+            <span class="nearby_item_content_tag">月售1万+</span>
+            <span class="nearby_item_content_tag">起送¥0</span>
+            <span class="nearby_item_content_tag">基础运费¥5</span>
+          </div>
+          <p class="nearby_item_content_highLight">VIP尊享满89元减4元运费券（每月3张）</p>
+        </div>
+      </div>
+      <div class="nearby_item">
+        <img class="nearby_item_img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="">
+        <div class="nearby_item_content">
+          <div class="nearby_item_content_title">沃尔玛</div>
+          <div class="nearby_item_content_tags">
+            <span class="nearby_item_content_tag">月售1万+</span>
+            <span class="nearby_item_content_tag">起送¥0</span>
+            <span class="nearby_item_content_tag">基础运费¥5</span>
+          </div>
+          <p class="nearby_item_content_highLight">VIP尊享满89元减4元运费券（每月3张）</p>
+        </div>
+      </div>
+      <div class="nearby_item">
+        <img class="nearby_item_img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="">
+        <div class="nearby_item_content">
+          <div class="nearby_item_content_title">沃尔玛</div>
+          <div class="nearby_item_content_tags">
+            <span class="nearby_item_content_tag">月售1万+</span>
+            <span class="nearby_item_content_tag">起送¥0</span>
+            <span class="nearby_item_content_tag">基础运费¥5</span>
+          </div>
+          <p class="nearby_item_content_highLight">VIP尊享满89元减4元运费券（每月3张）</p>
+        </div>
+      </div>
+      <div class="nearby_item">
+        <img class="nearby_item_img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="">
+        <div class="nearby_item_content">
+          <div class="nearby_item_content_title">沃尔玛</div>
+          <div class="nearby_item_content_tags">
+            <span class="nearby_item_content_tag">月售1万+</span>
+            <span class="nearby_item_content_tag">起送¥0</span>
+            <span class="nearby_item_content_tag">基础运费¥5</span>
+          </div>
+          <p class="nearby_item_content_highLight">VIP尊享满89元减4元运费券（每月3张）</p>
+        </div>
+      </div>
+      <div class="nearby_item">
+        <img class="nearby_item_img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="">
+        <div class="nearby_item_content">
+          <div class="nearby_item_content_title">沃尔玛</div>
+          <div class="nearby_item_content_tags">
+            <span class="nearby_item_content_tag">月售1万+</span>
+            <span class="nearby_item_content_tag">起送¥0</span>
+            <span class="nearby_item_content_tag">基础运费¥5</span>
+          </div>
+          <p class="nearby_item_content_highLight">VIP尊享满89元减4元运费券（每月3张）</p>
+        </div>
+      </div>
+    </div>
   </div>
   <div class="docker">
-    <span class="docker_item docker_item--active">
+    <div class="docker_item docker_item--active">
       <div class="iconfont">&#xe692;</div>
       <div class="docker_title">首页</div>
-    </span>
-    <span class="docker_item">
+    </div>
+    <div class="docker_item">
       <div class="iconfont">&#xe62e;</div>
       <div class="docker_title">购物车</div>
-      </span>
-    <span class="docker_item">
+    </div>
+    <div class="docker_item">
       <div class="iconfont">&#xe713;</div>
       <div class="docker_title">订单</div>
-    </span>
-    <span class="docker_item">
+    </div>
+    <div class="docker_item">
       <div class="iconfont">&#xe7ae;</div>
       <div class="docker_title">我的</div>
-    </span>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @import "./style/viriables";
 @import "./style/mixins";
+
 .wrapper {
+  overflow: auto;
   position: absolute;
   left: 0;
   top: 0;
   bottom: 0.5rem;
   right: 0;
   padding: 0 .19rem;
+
   .position {
     color: $content-fontcolor;
     position: relative;
@@ -43,11 +171,13 @@
     line-height: .22rem;
     font-size: .16rem;
     @include ellipsis;
+
     .position_icon {
       position: relative;
       top: .01rem;
       font-size: .2rem;
     }
+
     .position_notice {
       position: absolute;
       top: .17rem;
@@ -55,7 +185,112 @@
       font-size: .2rem;
     }
   }
+
+  .search {
+    margin-bottom: .12rem;
+    line-height: .32rem;
+    background-color: #F5F5F5;
+    color: #B7B7B7;
+    border-radius: .16rem;
+    font-size: .14rem;
+
+    .iconfont {
+      font-size: .2rem;
+      display: inline-block;
+      position: relative;
+      top: .02rem;
+      padding: 0 .01rem 0 .16rem;
+    }
+  }
+
+  .banner {
+    margin-bottom: .12rem;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 25.4%;
+
+    &_img {
+      width: 100%;
+    }
+  }
+
+  .icons {
+    display: flex;
+    flex-wrap: wrap;
+
+    &_item {
+      width: 20%;
+
+      &_img {
+        display: block;
+        height: .4rem;
+        width: .4rem;
+        margin: 0 auto;
+      }
+
+      &_desc {
+        text-align: center;
+        color: $content-fontcolor;
+      }
+    }
+  }
+
+  .gap {
+    height: .1rem;
+    background-color: $content-bgColor;
+    margin: 0 -.18rem;
+  }
+
+  .nearby {
+    &_title {
+      margin: .16rem 0 .04rem 0;
+      font-size: .18rem;
+      font-weight: normal;
+      color: $content-fontcolor;
+    }
+
+    &_item {
+      display: flex;
+      padding-top: .12rem;
+
+      &_img {
+        width: .56rem;
+        height: .56rem;
+        margin-right: .16rem;
+      }
+
+      &_content {
+        flex: 1;
+        border-bottom: 1px solid $content-bgColor;
+        padding-bottom: .12rem;
+
+        &_title {
+          line-height: .22rem;
+          font-size: .16rem;
+          color: $content-fontcolor;
+        }
+
+        &_tags {
+          line-height: .18rem;
+          font-size: .13rem;
+          color: $content-fontcolor;
+        }
+
+        &_tag {
+          margin-right: .16rem;
+        }
+
+        &_highLight {
+          margin: .08rem 0 0 0;
+          line-height: .18rem;
+          font-size: .13rem;
+          color: #E93B3B;
+        }
+      }
+    }
+  }
 }
+
 .docker {
   display: flex;
   position: absolute;
@@ -65,20 +300,24 @@
   bottom: 0;
   width: 100%;
   height: .49rem;
-  border-top: .01rem solid #F1F1F1;
-  box-shadow: 0 -1px 1px 0 #F1F1F1;
+  border-top: .01rem solid $content-bgColor;
+  box-shadow: 0 -1px 1px 0 $content-bgColor;
   color: $content-fontcolor;
+
   &_item {
     flex: 1;
     text-align: center;
+
     .iconfont {
       margin: 0 0 .02rem 0;
       font-size: .18rem;
     }
+
     &--active {
       color: #1FA4FC
     }
   }
+
   &_title {
     font-size: .2rem;
     //浏览器所能显示的最小像素为12px，如果要使用小于12px的像素，请使用transform
