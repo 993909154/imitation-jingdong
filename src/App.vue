2f74cf735c77999c5f97e5147a512ba64bbd1d4e
Along <993909154@qq.com>
2022-02-07 22:38:46 +0800

    css: top

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position_icon">&#xe7f1;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position_notice">&#xe613;</span>
+    </div>
+  </div>
   <div class="docker">
     <span class="docker_item docker_item--active">
       <div class="iconfont">&#xe692;</div>
@@ -20,6 +27,35 @@
 </template>
 
 <style lang="scss">
+@import "./style/viriables";
+@import "./style/mixins";
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem;
+  right: 0;
+  padding: 0 .19rem;
+  .position {
+    color: $content-fontcolor;
+    position: relative;
+    padding: .16rem .24rem .16rem 0;
+    line-height: .22rem;
+    font-size: .16rem;
+    @include ellipsis;
+    .position_icon {
+      position: relative;
+      top: .01rem;
+      font-size: .2rem;
+    }
+    .position_notice {
+      position: absolute;
+      top: .17rem;
+      right: 0;
+      font-size: .2rem;
+    }
+  }
+}
 .docker {
   display: flex;
   position: absolute;
@@ -29,12 +65,12 @@
   bottom: 0;
   width: 100%;
   height: .49rem;
-  border-top: 1px solid #F1F1F1;
+  border-top: .01rem solid #F1F1F1;
   box-shadow: 0 -1px 1px 0 #F1F1F1;
+  color: $content-fontcolor;
   &_item {
     flex: 1;
     text-align: center;
-    color: #333333;
     .iconfont {
       margin: 0 0 .02rem 0;
       font-size: .18rem;
@@ -44,7 +80,7 @@
     }
   }
   &_title {
-    font-size: 20px;
+    font-size: .2rem;
     //浏览器所能显示的最小像素为12px，如果要使用小于12px的像素，请使用transform
     transform: scale(0.5, 0.5);
     transform-origin: center top;
